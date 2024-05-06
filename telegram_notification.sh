1a04bdb189d7b6b722ebb416d68ec74b1c97f019
giobmoro <94879073+giobmoro@users.noreply.github.com>
2024-05-05 23:51:17 -0300

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-# Construa a URL do bot do Telegram usando a variável de ambiente TELEGRAM_TOKEN:
 BOT_URL="https://api.telegram.org/bot6666920619:AAFSFaxDkht8AaScjkbBM-P1VrhTYQl2sT0/sendMessage"
-
-# Defina o formato da mensagem. Pode ser "Markdown" ou "HTML":
 PARSE_MODE="Markdown"
 
-# Envie a mensagem usando o GitHub API:
 send_msg () {
     curl -s -X POST "${BOT_URL}" -d chat_id="1472375243" \
         -d text="$1" -d parse_mode="${PARSE_MODE}"
 }
 
-# Defina a mensagem a ser enviada:
 msg="
 -------------------------------------
 Send message to telegram!
